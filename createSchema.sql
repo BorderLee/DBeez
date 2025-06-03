616b3eed2ab6971fe94254ac73f2429a6734d2be
tykim0O0 <emily9943@naver.com>
2025-06-03 17:48:55 +0900

    cake_rate 삭제, init 및 create 순서 변경(바로 복붙 가능하게), constraint 추가가

--- createSchema.sql
+++ createSchema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE Cakes (
   cake_id 			CHAR(3) PRIMARY KEY,
   cake_name 		VARCHAR(100),
-  price 			INT,
-  cake_rate 		NUMERIC(2,1) NULL
+  price 			INT
 );
 
 CREATE INDEX cakes_index ON Cakes(cake_id);
@@ -20,7 +19,7 @@ CREATE TABLE Stores(
 CREATE INDEX stores_index ON Stores(store_id);
 
 CREATE TABLE Customers (
-    customer_id 	INT PRIMARY KEY, 
+    customer_id 	INT PRIMARY KEY,
     customer_name 	VARCHAR(100) NOT NULL, 
     address	      VARCHAR(255), 
     phone_number 	VARCHAR(20),
@@ -29,21 +28,6 @@ CREATE TABLE Customers (
 
 CREATE INDEX customers_index on Customers(customer_id);
 
-CREATE TABLE Reviews
-	(review_num		CHAR(8) PRIMARY KEY, 
-	 customer_id	INT,
-	 review_rate	NUMERIC(2,1) NULL,
-	 cake_id		CHAR(3),
-	 order_num		INT (5),
-	 store_id		CHAR(5),
-	 foreign key (customer_id) references Customers(customer_id),
-	 foreign key (cake_id) references Cakes(cake_id),
-	 foreign key (order_num) references Orders(order_num),
-	 foreign key (store_id) references Stores(store_id)
-);
-
-CREATE INDEX reviews_index on Reviews(review_num);
-
 CREATE TABLE Orders(
     order_num       INT(5) PRIMARY KEY,
     order_date      DATETIME NOT NULL,
@@ -51,12 +35,27 @@ CREATE TABLE Orders(
     cake_id	        CHAR(3),
     store_id        CHAR(5),
     price           INT,
-    foreign key (customer_id) references Customers(customer_id),
-    foreign key (cake_id) references Cakes(cake_id),
-    foreign key (store_id) references Stores(store_id)
+    foreign key (customer_id) references Customers(customer_id) on delete set NULL on update CASCADE,
+    foreign key (cake_id) references Cakes(cake_id) on delete set NULL on update CASCADE,
+    foreign key (store_id) references Stores(store_id) on delete set NULL on update CASCADE
 );
 
 CREATE INDEX orders_index on Orders(order_num);
+
+CREATE TABLE Reviews
+	(review_num		CHAR(8) PRIMARY KEY, 
+	 customer_id	INT,
+	 review_rate	NUMERIC(2,1) NULL,
+	 cake_id		CHAR(3),
+	 order_num		INT (5),
+	 store_id		CHAR(5),
+	 foreign key (customer_id) references Customers(customer_id) on delete set NULL on update CASCADE,
+	 foreign key (cake_id) references Cakes(cake_id) on delete set NULL on update CASCADE,
+	 foreign key (order_num) references Orders(order_num) on delete set NULL on update CASCADE,
+	 foreign key (store_id) references Stores(store_id) on delete set NULL on update CASCADE
+);
+
+CREATE INDEX reviews_index on Reviews(review_num);
 
 -- view (cake rating 계산)
 CREATE VIEW cake_rate_cal AS
@@ -68,5 +67,3 @@ GROUP BY c.cake_id;
 CREATE VIEW review_info AS
 SELECT cus.customer_name, r.review_rate, cake.cake_name, s.store_name
 FROM Reviews r JOIN Customers cus ON r.customer_id=cus.customer_id JOIN Cakes cake ON r.cake_id=cake.cake_id JOIN Stores s ON r.store_id=s.store_id;
-=======
-CREATE INDEX orders_index on Orders(order_num);
